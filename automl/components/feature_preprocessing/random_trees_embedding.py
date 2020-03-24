b6735a79e3ef83eb58f7361fbe59200b76401679
tim <tim.grams@usu.com>
2020-03-24 16:53:32 +0100

    added max_depth_factors and max_leaf_nodes_factors

--- automl/components/feature_preprocessing/random_trees_embedding.py
+++ automl/components/feature_preprocessing/random_trees_embedding.py
@@ -5,15 +5,17 @@ from ConfigSpace.hyperparameters import UniformIntegerHyperparameter, \
 from automl.components.base import PreprocessingAlgorithm
 from automl.util.common import check_none, check_for_bool
 
+from util.common import resolve_factor
+
 
 class RandomTreesEmbeddingComponent(PreprocessingAlgorithm):
     def __init__(self,
                  n_estimators: int = 100,
-                 max_depth: int = 5,
+                 max_depth_factor: int = 5,
                  min_samples_split: int = 2,
                  min_samples_leaf: int = 1,
                  min_weight_fraction_leaf: float = 0.,
-                 max_leaf_nodes: int = None,
+                 max_leaf_nodes_factor: int = None,
                  min_impurity_decrease: float = 0.,
                  random_state=None,
                  n_jobs: int = None,
@@ -21,11 +23,11 @@ class RandomTreesEmbeddingComponent(PreprocessingAlgorithm):
                  ):
         super().__init__()
         self.n_estimators = n_estimators
-        self.max_depth = max_depth
+        self.max_depth_factor = max_depth_factor
         self.min_samples_split = min_samples_split
         self.min_samples_leaf = min_samples_leaf
         self.min_weight_fraction_leaf = min_weight_fraction_leaf
-        self.max_leaf_nodes = max_leaf_nodes
+        self.max_leaf_nodes_factor = max_leaf_nodes_factor
         self.min_impurity_decrease = min_impurity_decrease
         self.n_jobs = n_jobs
         self.random_state = random_state
@@ -36,31 +38,23 @@ class RandomTreesEmbeddingComponent(PreprocessingAlgorithm):
         from sklearn.ensemble import RandomTreesEmbedding
 
         self.n_estimators = int(self.n_estimators)
-        if check_none(self.max_depth):
-            self.max_depth = None
-        else:
-            self.max_depth = int(self.max_depth)
         self.min_samples_split = int(self.min_samples_split)
         self.min_samples_leaf = int(self.min_samples_leaf)
-        if check_none(self.max_leaf_nodes):
-            self.max_leaf_nodes = None
-        else:
-            self.max_leaf_nodes = int(self.max_leaf_nodes)
         self.min_weight_fraction_leaf = float(self.min_weight_fraction_leaf)
         self.bootstrap = check_for_bool(self.bootstrap)
 
-        if self.max_depth == 1:
-            self.max_depth = None
+        # Heuristic to set the tree depth
+        max_depth = resolve_factor(self.max_depth_factor, X.shape[1])
 
-        if self.max_leaf_nodes == 1:
-            self.max_leaf_nodes = None
+        # Heuristic to set the tree width
+        max_leaf_nodes = resolve_factor(self.max_leaf_nodes_factor, X.shape[0])
 
         self.preprocessor = RandomTreesEmbedding(
             n_estimators=self.n_estimators,
-            max_depth=self.max_depth,
+            max_depth=max_depth,
             min_samples_split=self.min_samples_split,
             min_samples_leaf=self.min_samples_leaf,
-            max_leaf_nodes=self.max_leaf_nodes,
+            max_leaf_nodes=max_leaf_nodes,
             min_weight_fraction_leaf=self.min_weight_fraction_leaf,
             min_impurity_decrease=self.min_impurity_decrease,
             sparse_output=False,
@@ -114,13 +108,13 @@ class RandomTreesEmbeddingComponent(PreprocessingAlgorithm):
         # TODO check max_depth and max_leaf_nodes
 
         n_estimators = UniformIntegerHyperparameter(name="n_estimators", lower=10, upper=400, default_value=10)
-        max_depth = UniformIntegerHyperparameter(name="max_depth", lower=2, upper=50, default_value=5)
+        max_depth_factor = UniformFloatHyperparameter("max_depth_factor", 1e-5, 1., default_value=1.)
         min_samples_split = UniformFloatHyperparameter("min_samples_split", 0.0001, 0.5, default_value=0.0001)
         min_samples_leaf = UniformFloatHyperparameter("min_samples_leaf", 0.0001, 0.5, default_value=0.0001)
         min_weight_fraction_leaf = UniformFloatHyperparameter("min_weight_fraction_leaf", 0., 0.5, default_value=0.)
-        max_leaf_nodes = UniformIntegerHyperparameter("max_leaf_nodes", 1, 100, default_value=1)
+        max_leaf_nodes_factor = UniformFloatHyperparameter("max_leaf_nodes_factor", 1e-5, 1., default_value=1.)
         min_impurity_decrease = UniformFloatHyperparameter('min_impurity_decrease', 0., 0.75, default_value=0.)
 
-        cs.add_hyperparameters([n_estimators, max_depth, min_samples_split, min_samples_leaf,
-                                min_weight_fraction_leaf, max_leaf_nodes, min_impurity_decrease])
+        cs.add_hyperparameters([n_estimators, max_depth_factor, min_samples_split, min_samples_leaf,
+                                min_weight_fraction_leaf, max_leaf_nodes_factor, min_impurity_decrease])
         return cs
