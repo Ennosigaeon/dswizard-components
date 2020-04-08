Fix identification of numerical and categorical data

automl/components/data_preprocessing/imputation.py
@@ -18,27 +18,19 @@ class ImputationComponent(PreprocessingAlgorithm):
         from sklearn.impute import SimpleImputer
         from sklearn.compose import ColumnTransformer
 
-        cat = []
-        num = []
-
         if not np.any(pd.isna(X)):
             self.preprocessor = NoopComponent()
             return self
         else:
-            X_object = X.select_dtypes(include=['category', 'object'])
-            X_numeric = X._get_numeric_data()
-
-            for i in range(X_object.shape[1]):
-                cat.append(X_object.columns[i])
-            for i in range(X_numeric.shape[1]):
-                num.append(X_numeric.columns[i])
+            numeric = X.select_dtypes(include=['number']).columns
+            categorical = X.select_dtypes(include=['category', 'object']).columns
 
         self.preprocessor = ColumnTransformer(
             transformers=[
                 ('cat', SimpleImputer(missing_values=self.missing_values, strategy='most_frequent',
-                                      add_indicator=False, copy=False), cat),
+                                      add_indicator=False, copy=False), categorical),
                 ('num', SimpleImputer(missing_values=self.missing_values, strategy=self.strategy,
-                                      add_indicator=False, copy=False), num)
+                                      add_indicator=False, copy=False), numeric)
             ]
         )
         self.preprocessor.fit(X)

automl/components/feature_preprocessing/multi_column_label_encoder.py
@@ -22,21 +22,18 @@ class MultiColumnLabelEncoderComponent(PreprocessingAlgorithm):
         LabelEncoder(). If no columns specified, transforms all
         columns in X.
         """
-        X_object = X.select_dtypes(include=['category', 'object'])
 
-        if X_object.empty:
+        categorical = X.select_dtypes(include=['category', 'object']).columns
+        if len(categorical) == 0:
             return X.to_numpy()
         else:
-            for colname, col in X.iteritems():
-                if colname in X_object.columns:
-                    missing_vec = pd.isna(X[colname])
-                    missing_vec = missing_vec[missing_vec == True]
-                    X[colname] = X[colname].cat.add_categories(['<MISSING>'])
-                    X.loc[missing_vec.index, colname] = '<MISSING>'
+            for colname in categorical:
+                missing_vec = pd.isna(X[colname])
+                X[colname] = X[colname].cat.add_categories(['<MISSING>'])
+                X.loc[missing_vec, colname] = '<MISSING>'
 
-                    X[colname] = self.preprocessor.fit_transform(col.astype(str))
-
-                    X.loc[missing_vec.index, colname] = np.nan
+                X[colname] = self.preprocessor.fit_transform(X[colname].astype(str))
+                X.loc[missing_vec, colname] = np.nan
 
         return X.to_numpy()
 

automl/components/feature_preprocessing/one_hot_encoding.py
@@ -12,23 +12,8 @@ class OneHotEncoderComponent(PreprocessingAlgorithm):
         return self
 
     def transform(self, X: pd.DataFrame):
-
-        dummy_na = False
-        cat = []
-
-        if np.any(pd.isna(X)):
-            dummy_na = True
-
-        X_object = X.select_dtypes(include=['category', 'object'])
-
-        for i in range(X_object.shape[1]):
-            cat.append(X_object.columns[i])
-
-        if len(cat) == 0:
-            return X.to_numpy()
-
-        X = pd.get_dummies(X, prefix=[cat[i] for i in range(len(cat))], sparse=False, dummy_na=dummy_na)
-
+        dummy_na = np.any(pd.isna(X))
+        X = pd.get_dummies(X, sparse=False, dummy_na=dummy_na)
         return X.to_numpy()
 
     @staticmethod

tests/test_label_encoder.py
@@ -18,16 +18,16 @@ class TestLabelEncoderComponent(base_test.BaseComponentTest):
         assert np.allclose(X_actual, X_test)
 
     def test_categorical(self):
-
         actual = MultiColumnLabelEncoderComponent()
-        X_before = pd.DataFrame([['Mann', 1], ['Frau', 2], ['Frau', 1]], dtype='category')
+        X_before = pd.DataFrame([['Mann', np.nan], ['Frau', 2], [None, 1]], columns=['Gender', 'Label'],
+                                dtype='category')
         y_before = pd.Series([1, 1, 0])
         actual.fit(X_before, y_before)
         X_after = actual.transform(X_before).astype(float)
 
-        X_test = np.array([[1.0, 1], [0.0, 2], [0.0, 1]])
+        X_test = np.array([[2.0, np.nan], [1.0, 1.0], [np.nan, 0.0]])
 
-        assert np.allclose(X_after, X_test)
+        assert np.allclose(X_after, X_test, equal_nan=True)
 
     def test_configured(self):
         pass