a9c423a4700c135eb499ba97f9ab8588ab0e41d5
Marc Zoeller <m.zoeller@usu.de>
2021-06-03 11:36:44 +0200

    Fix various IDE warnings

--- dswizard/components/util.py
+++ dswizard/components/util.py
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-import importlib
 from typing import Optional
 
 import numpy as np
@@ -86,22 +85,6 @@ def prefixed_name(prefix: Optional[str], name: str) -> str:
     Returns the potentially prefixed name name.
     """
     return name if prefix is None else f'{prefix}:{name}'
-
-
-def get_type(clazz: str) -> type:
-    module_name = clazz.rpartition(".")[0]
-    class_name = clazz.split(".")[-1]
-
-    module = importlib.import_module(module_name)
-    class_ = getattr(module, class_name)
-    return class_
-
-
-def get_object(clazz: str, kwargs=None):
-    if kwargs is None:
-        kwargs = {}
-
-    return get_type(clazz)(**kwargs)
 
 
 def object_log(X: np.ndarray):
